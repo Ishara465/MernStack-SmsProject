210292b53aca47ebff2e2c634caa4cf03eefd6b5
Ishara465 <116372693+Ishara465@users.noreply.github.com>
2025-01-29 23:20:12 +0530

    create logout

--- client/src/components/NavigationBar/NavigationBar.jsx
+++ client/src/components/NavigationBar/NavigationBar.jsx
@@ -5,26 +5,34 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 const NavigationBar = () => {
   return (
     <div>
-      <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="/">Student Management System</Navbar.Brand>
+      <Navbar bg="dark" data-bs-theme="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="/home">Student Management System</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          {/* Left-aligned links */}
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/home">Home</Nav.Link>
             <NavDropdown title="Managements" id="nav-dropdown">
-              <NavDropdown.Item eventKey="4.1" href="/student">Student Management</NavDropdown.Item>
-              <NavDropdown.Item eventKey="4.2" href="/tutor">Tutor Management</NavDropdown.Item>
-              <NavDropdown.Item eventKey="4.2" href="/classMng">Class Management</NavDropdown.Item>       
+              <NavDropdown.Item href="/student">Student Management</NavDropdown.Item>
+              <NavDropdown.Item href="/tutor">Tutor Management</NavDropdown.Item>
+              <NavDropdown.Item href="/classMng">Class Management</NavDropdown.Item>       
             </NavDropdown>
 
             <NavDropdown title="Others" id="nav-dropdown">
-              <NavDropdown.Item eventKey="4.1" href="/attendanceMg">Student Attendance</NavDropdown.Item>
-              <NavDropdown.Item eventKey="4.2" href="/eventMng">Event And Announcement</NavDropdown.Item>
-              <NavDropdown.Item eventKey="4.2" href="/classFee">Class Fee</NavDropdown.Item>            
+              <NavDropdown.Item href="/attendanceMg">Student Attendance</NavDropdown.Item>
+              <NavDropdown.Item href="/eventMng">Event And Announcement</NavDropdown.Item>
+              <NavDropdown.Item href="/classFee">Class Fee</NavDropdown.Item>            
             </NavDropdown>
-            
           </Nav>
-        </Container>
-      </Navbar>
+
+          {/* Right-aligned LogOut button */}
+          <Nav className="ms-auto">
+            <Nav.Link href="/login" style={{fontWeight:"bold",color:"white", fontSize:"20px"}}>Logout</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     </div>
   );
 };
